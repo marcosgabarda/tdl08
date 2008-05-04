2c796e2c331f6a9afef592480c562dcb479634a8
cyberslas <cyberslas@5b558abe-654c-0410-8053-5d03c91816a9>
2008-05-04 11:19:00 +0000

    Primera release subida.

---
+++ AFD.cpp
@@ -0,0 +1,447 @@
+#include "AFD.h"
+#include <list>
+#include <vector>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <set>
+#include <queue>
+#include <fstream>
+
+#ifdef WINDOWS
+#define SRANDOM srand
+#define RANDOM rand
+#else
+#define SRANDOM srandom
+#define RANDOM random
+#endif
+
+using namespace std;
+
+
+bool fileExists(const std::string& fileName)
+{
+  std::fstream fin;
+  fin.open(fileName.c_str(),std::ios::in);
+  if( fin.is_open() )
+  {
+    fin.close();
+    return true;
+  }
+  fin.close();
+  return false;
+}
+
+bool AFD::salvar(const std::string& strFileName) const {
+  std::ofstream osSalida;
+  
+  if (fileExists(strFileName))
+  {
+    // Si el archivo existe, entonces sólo se pueden añadir AFDs con nombre
+    if(m_strName.empty())
+      return false;
+
+    osSalida.open(strFileName.c_str(),std::ios_base::app);
+  }
+  else
+  {
+    osSalida.open(strFileName.c_str());
+  }
+
+  if(!m_strName.empty())
+    osSalida << "Name " << m_strName << std::endl;
+  
+  osSalida << "NumStates " << m_cEstados << std::endl;
+  
+  for(int iEstado=0; iEstado<m_cEstados; iEstado++)
+    osSalida << "State " << iEstado << " initial=" << (m_iEstadoInicial==iEstado) << " final=" << (m_vbEstadosFinales[iEstado] ? 1:0) << std::endl;
+  
+  for(int iEstadoOrigen=0; iEstadoOrigen<m_cEstados; iEstadoOrigen++)
+    for(int iSimbolo=0; iSimbolo<m_cSimbolos; iSimbolo++)
+      osSalida << iEstadoOrigen << " " << (m_lTransiciones.find(Par(iEstadoOrigen,iSimbolo)))->second << " " << m_vcAlfabeto[iSimbolo] << std::endl;
+  osSalida.close();
+  return true;
+}
+
+bool AFD::hayEstadosNoAlcanzables() const {
+
+  int iEstadoActual = m_iEstadoInicial;
+
+  std::queue<int> lEstados;
+  std::set<int> lEstadosVisitados;
+
+  lEstados.push(iEstadoActual);
+  lEstadosVisitados.insert(iEstadoActual);
+
+  while (!lEstados.empty()) {
+
+    iEstadoActual = lEstados.front();
+    lEstados.pop();
+
+    for (int i = 0; i < m_cSimbolos; i++) {
+      if (m_lTransiciones.find(Par(iEstadoActual,i)) != m_lTransiciones.end()) {
+        int iEstadoDestino = m_lTransiciones.find(Par(iEstadoActual,i))->second;
+	      if (lEstadosVisitados.find(iEstadoDestino) == lEstadosVisitados.end()) {
+	        lEstados.push(iEstadoDestino);
+	        lEstadosVisitados.insert(iEstadoDestino);
+	      }
+      }
+    }    
+  }
+
+  if (lEstadosVisitados.size() != m_cEstados) 
+    return true;
+  return false;
+
+}
+
+void AFD::inicializar() {
+
+  /*
+   * Inicializamos la semilla aleatroria.
+   */
+
+  SRANDOM(static_cast<unsigned int>(time(0)));
+
+  for (int iEstado = 0; iEstado < m_cEstados; iEstado++) {
+    for (int iSimbolo = 0; iSimbolo < m_cSimbolos; iSimbolo++) {
+      int iEstadoDestino = RANDOM()%m_cEstados;
+      m_lTransiciones[Par(iEstado,iSimbolo)]=iEstadoDestino;
+    }
+  }
+
+  /*
+   * Inicializamos la semilla aleatroria.
+   */
+  SRANDOM(static_cast<unsigned int>(time(0)));
+
+  for (int i = 0; i < m_cEstados; i++) {
+    if(RANDOM()%2 == 0) {
+      m_vbEstadosFinales[i] = true;
+    } else {
+      m_vbEstadosFinales[i] = false;
+    }
+  }
+
+  /* 
+   * Queremos garantizar que haya al menos un estado final
+   */
+  m_vbEstadosFinales[RANDOM()%m_cEstados]=true;
+
+  /*
+   * Inicializamos la semilla aleatroria.
+   */
+  SRANDOM(static_cast<unsigned int>(time(0)));
+
+  m_iEstadoInicial = RANDOM()%m_cEstados;
+
+  if (hayEstadosNoAlcanzables()) { // || minimizar().getNumEstados()!=m_cEstados) {
+    // std::cerr << "Warning - Se encontraron estados no alcanzables." << std::endl;
+    m_lTransiciones.clear();
+  }
+}
+
+AFD AFD::minimizar() const {
+  
+  /*
+   * El algoritmo comienza con una particion de estados entre finales y no finales,
+   * y va refinando la particion. Cuando la particion no puede refirnarse mas,
+   * todos los estados de un compartimento de la particion pueden ser fusionados en
+   * uno solo.
+   */
+  
+  Particion particion;
+  bool bCambio = true;
+
+  /*
+   * Particion inicial
+   */
+
+  for (int i = 0; i < m_cEstados; i++) {
+    Particion::t_IDCompartimento lNombreParticion;
+    lNombreParticion.push_back(m_vbEstadosFinales[i] ? 0 : 1);
+    particion[lNombreParticion].push_back(i);
+  }
+
+  do {
+    Particion nuevaParticion;
+    for(int iEstado = 0; iEstado < m_cEstados; iEstado++ ) {
+      Particion::t_IDCompartimento lNombreParticion = particion.obtenerCompartimento(iEstado);
+      
+      /*
+       * Si la particion solo tiene mas de un estado, es susceptible de refinarse mas
+       */
+      if(particion[lNombreParticion].size() > 1)
+        /*
+         * Calculamos el nombre de la nueva particion
+         */
+        for (int iSimbolo = 0; iSimbolo < m_cSimbolos; iSimbolo++)
+	  //          lNombreParticion.merge(particion.obtenerCompartimento(m_lTransiciones.find(Par(iEstado,iSimbolo))->second));
+      
+      nuevaParticion[lNombreParticion].push_back(iEstado);
+    }
+
+    if(nuevaParticion == particion)
+      bCambio=false;
+
+    particion = nuevaParticion;
+    
+  } while (bCambio);
+  
+  /*
+   * En este punto la particion se ha refinado al maximo
+   */
+
+  /*
+   * Si la particion esta refinada al maximo
+   */
+  if(particion.getSize() == static_cast<unsigned int>(m_cEstados))
+    return *this; // Devolvemos el AFD actual, porque es minimo
+
+  std::map<Par,int> lTransicionesMinimas;
+  std::vector<bool> vbEstadosMinimosFinales(particion.getSize());
+  int iNuevoEstado = 0;
+
+  for(Particion::const_iterator itCompartimento=particion.begin(); itCompartimento != particion.end(); itCompartimento++, iNuevoEstado++) {
+    for(int iSimbolo = 0; iSimbolo < m_cSimbolos; iSimbolo++) {
+      lTransicionesMinimas[Par(iNuevoEstado,iSimbolo)] = particion.obtenerIndice(m_lTransiciones.find(Par(*((itCompartimento->second).begin()),iSimbolo))->second);
+    }
+    vbEstadosMinimosFinales[iNuevoEstado] = m_vbEstadosFinales[*((itCompartimento->second).begin())]; // Seguro que esta l�nea se puede optimizar un 1000%
+  }
+
+  AFD afdMinimo(m_cSimbolos, static_cast<int>(particion.getSize()), lTransicionesMinimas, vbEstadosMinimosFinales, particion.obtenerIndice(m_iEstadoInicial));
+
+
+  return afdMinimo;  
+}
+
+AFD AFD::minimizar2() const {
+  std::vector<std::set<int> > P(2);
+  std::vector<std::set<int> > P2;
+  std::set<std::set<int> > L;
+  std::set<int> noFinales;
+
+  for(int iEstado=0; iEstado<m_cEstados; iEstado++)
+  {
+    P[m_vbEstadosFinales[iEstado] ? 0 : 1].insert(iEstado);
+    if(!m_vbEstadosFinales[iEstado])
+      noFinales.insert(iEstado);
+  }
+  L.insert(noFinales);
+
+  while(!L.empty())
+  {
+    std::set<int> S = *(L.begin());
+    L.erase(L.begin());
+
+    for(int iSimbolo=0; iSimbolo<m_cSimbolos; iSimbolo++)
+    {
+      P2.clear();
+      for(std::vector<std::set<int> >::iterator B = P.begin();
+          B != P.end();
+          B++)
+      {
+        std::set<int> B1, B2;
+        // Split
+        for(std::set<int>::iterator b = B->begin();
+            b != B->end();
+            b++)
+        {
+          if(S.find(m_lTransiciones.find(Par(*b,iSimbolo))->second)!=S.end())
+            B1.insert(*b);
+          else
+            B2.insert(*b);
+        } // endfor b
+
+        if(!B1.empty())
+          P2.push_back(B1);
+        if(!B2.empty())
+          P2.push_back(B2);
+
+        if(!B1.empty() && !B2.empty())
+        {
+          if(B1.size() < B2.size())
+            L.insert(B1);
+          else
+            L.insert(B2);
+        }
+      } // endfor B
+      P=P2;
+    } // endfor iSimbolo
+  } //endwhile
+
+  // En este punto la partición está refinada al máximo
+
+  std::vector<int> vIndices(m_cEstados);
+  int iPedo=0;
+  for(std::vector<std::set<int> >::const_iterator itCompartimento=P.begin();
+      itCompartimento != P.end();
+      itCompartimento++, iPedo++)
+  {
+    for(std::set<int>::const_iterator itElem = itCompartimento->begin();
+        itElem != itCompartimento->end();
+        itElem++)
+    {
+      vIndices[*itElem]=iPedo;
+    }
+  }
+
+  /*
+  * Si la particion esta refinada al maximo
+  */
+  if(P.size() == static_cast<size_t>(m_cEstados))
+    return *this; // Devolvemos el AFD actual, porque es minimo
+
+  std::map<Par,int> lTransicionesMinimas;
+  std::vector<bool> vbEstadosMinimosFinales(P.size());
+  int iNuevoEstado = 0;
+
+  for(std::vector<std::set<int> >::const_iterator itCompartimento=P.begin();
+      itCompartimento != P.end();
+      itCompartimento++, iNuevoEstado++) {
+    for(int iSimbolo = 0; iSimbolo < m_cSimbolos; iSimbolo++) {
+      lTransicionesMinimas[Par(iNuevoEstado,iSimbolo)] = vIndices[m_lTransiciones.find(Par(*(itCompartimento->begin()),iSimbolo))->second];
+    }
+    vbEstadosMinimosFinales[iNuevoEstado] = m_vbEstadosFinales[*(itCompartimento->begin())];
+  }
+
+  AFD afdMinimo(m_cSimbolos, static_cast<int>(P.size()), lTransicionesMinimas, vbEstadosMinimosFinales, vIndices[m_iEstadoInicial]);
+
+
+  return afdMinimo;  
+
+}
+
+std::set<std::set<int> > AFD::calculaEstadosDR() const {
+  std::map<Par,std::set<int> > transicionesR;
+  std::map<std::pair<std::set<int>,int>,std::set<int> > transicionesDR;
+  std::set<std::set<int> > estadosDR;
+  std::list<std::set<int> > porProcesar;
+
+  for(std::map<Par,int>::const_iterator it = m_lTransiciones.begin();
+      it != m_lTransiciones.end();
+      it++)
+  {
+    transicionesR[Par(it->second,it->first.second)].insert(it->first.first);
+  }
+
+  {
+    std::set<int> estadoInicialDR;
+    for(int i=0; i<m_cEstados; i++)
+      if(m_vbEstadosFinales[i])
+        estadoInicialDR.insert(i);
+
+    porProcesar.push_back(estadoInicialDR);
+  }
+
+  while(!porProcesar.empty()) {
+    std::set<int> estadoActual(porProcesar.front());
+    porProcesar.pop_front();
+
+    if(estadosDR.find(estadoActual) != estadosDR.end())
+      continue;
+
+    for(int iSimbolo=0; iSimbolo < m_cSimbolos; iSimbolo++) {
+      std::set<int> destino;
+      for(std::set<int>::const_iterator itEstadoActual = estadoActual.begin();
+          itEstadoActual != estadoActual.end();
+          itEstadoActual++)
+      {
+        std::set<int> aux = transicionesR[Par(*itEstadoActual,iSimbolo)];
+        for(std::set<int>::const_iterator itAux = aux.begin();
+            itAux != aux.end();
+            itAux++)
+        {
+          destino.insert(*itAux);
+        } // endfor aux
+      } // endfor estadoActual
+      transicionesDR[std::pair<std::set<int>,int>(estadoActual,iSimbolo)] = destino;
+      if(!destino.empty() && destino != estadoActual && estadosDR.find(destino)==estadosDR.end())
+        porProcesar.push_back(destino);
+    } // endfor simbolos
+    estadosDR.insert(estadoActual);
+  } // endwhile
+
+  return estadosDR;
+}
+
+AFD::Particion::t_IDCompartimento AFD::Particion::obtenerCompartimento(const int iEstado) const {
+  for(Particion::const_iterator itCompartimento = this->begin(); itCompartimento != this->end(); itCompartimento++) {
+    t_ListaEstados lEstados=itCompartimento->second;
+    for(t_ListaEstados::const_iterator itEstados = lEstados.begin(); itEstados != lEstados.end(); itEstados++) {
+      if (*itEstados == iEstado)
+        return itCompartimento->first;
+    }
+  }
+
+  throw new std::exception();
+}
+
+int AFD::Particion::obtenerIndice(const int iEstado) const {
+  int iIndice = 0;
+  for(Particion::const_iterator itCompartimento = this->begin(); itCompartimento != this->end(); itCompartimento++, iIndice++) {
+    t_ListaEstados lEstados=itCompartimento->second;
+    for(t_ListaEstados::const_iterator itEstados = lEstados.begin(); itEstados != lEstados.end(); itEstados++) {
+      if (*itEstados == iEstado)
+        return iIndice;
+    }
+  }
+
+  throw new std::exception();
+}
+
+bool AFD::Particion::operator ==(const Particion &rhs) const {
+
+  if(this->getSize() != rhs.getSize())
+    return false;
+
+  Particion::const_iterator itParticion1(this->begin());
+  Particion::const_iterator itParticion2(rhs.begin());
+
+  for(; itParticion1!=this->end() && itParticion2!=rhs.end(); itParticion1++, itParticion2++) {
+    if(itParticion1->second != itParticion2->second)
+      return false;
+  }
+  
+  return true;
+}
+
+void AFD::setAlfabeto(std::list<char> lAlfabeto) {
+  /*
+   * El método solo cogera de la lista de simbolos los indicados por el numero de simbolos
+   * que se haya definido en el constructor. Se usará para definir el alfabeto si no se ha 
+   * definido en el constructor.
+   */
+  m_lciAlfabeto.clear();
+  m_vcAlfabeto.clear();
+  std::list<char>::iterator it = lAlfabeto.begin();
+  for (int i = 0; i < m_cSimbolos; i++, it++) {
+    m_lciAlfabeto[*it] = i;
+    m_vcAlfabeto[i] = *it;
+  } 
+}
+
+std::list<char> AFD::getAlfabeto () const {
+  std::list<char> lAlfabeto;
+  unsigned int nSize = m_vcAlfabeto.size();
+  for (unsigned int i = 0; i < nSize; i++) {
+    lAlfabeto.push_back(m_vcAlfabeto[i]);
+  }
+  return lAlfabeto;  
+}
+
+bool AFD::evaluar (std::string strCadena) const {
+  
+  int iEstadoActual = m_iEstadoInicial;
+  int nSimbolos = strCadena.size();
+
+  for(int iSimbolo = 0; iSimbolo < nSimbolos; iSimbolo++) {
+    if (m_lciAlfabeto.find(strCadena[iSimbolo]) == m_lciAlfabeto.end()) return false;
+    if (m_lTransiciones.find(Par(iEstadoActual, (m_lciAlfabeto.find(strCadena[iSimbolo]))->second)) == m_lTransiciones.end()) return false;
+    iEstadoActual = m_lTransiciones.find(Par(iEstadoActual, (m_lciAlfabeto.find(strCadena[iSimbolo]))->second))->second;
+  }
+
+  return m_vbEstadosFinales[iEstadoActual];
+}
---
+++ AFD.h
@@ -0,0 +1,219 @@
+#ifndef AFD_H_
+#define AFD_H_
+
+#define WINDOWS
+
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <cstdlib>
+
+typedef std::pair<int,int> Par;
+typedef std::pair<int,char> cPar;
+
+/*
+ * Clase que implementa un Automata Finito Determinista. 
+ */
+class AFD
+{
+ public:
+
+   int getNumEstados() {return m_cEstados;}
+
+  /*
+   * El constructor aleatorio requiere de cSimbolos="nº de simbolos" y de cEstados="nº de estados"
+   */
+ AFD(int cSimbolos, int cEstados):
+  m_cSimbolos(cSimbolos),
+    m_cEstados(cEstados),
+    m_vbEstadosFinales(cEstados),
+    m_strName("")
+    {
+      do {
+        inicializar();
+      } while (m_lTransiciones.empty());
+    }
+
+  /*
+   * El constructor definido requiere:
+   *  cSimbolos="nº de simbolos",
+   *  cEstados="nº de estados"
+   *  lTransiciones="definicion de las transiciones"
+   *  vbEstadosFinales="definición de estados finales"
+   *  
+   */
+ AFD(int cSimbolos, int cEstados, std::map<Par,int> lTransiciones, std::vector<bool> vbEstadosFinales, int iEstadoInicial):
+  m_cSimbolos(cSimbolos),
+    m_cEstados(cEstados),
+    m_vbEstadosFinales(vbEstadosFinales),
+    m_lTransiciones(lTransiciones),
+    m_iEstadoInicial(iEstadoInicial),
+    m_strName("")
+    {
+    }
+
+AFD(std::set<char> lSimbolos, int cEstados, std::map<cPar,int> lTransiciones, std::vector<bool> vbEstadosFinales, int iEstadoInicial):
+  m_cSimbolos(static_cast<int>(lSimbolos.size())),
+    m_cEstados(cEstados),
+    m_vbEstadosFinales(vbEstadosFinales),
+    m_iEstadoInicial(iEstadoInicial),
+    m_vcAlfabeto(static_cast<int>(lSimbolos.size()))
+    {
+      std::set<char>::iterator it = lSimbolos.begin();
+      for (int i = 0; i < m_cSimbolos; i++, it++) {
+        m_lciAlfabeto[*it] = i;
+        m_vcAlfabeto[i] = *it;
+      }
+      std::map<cPar,int>::iterator itM;
+      for (itM = lTransiciones.begin(); itM != lTransiciones.end(); itM++) {
+        m_lTransiciones[Par((itM->first).first, m_lciAlfabeto[(itM->first).second])] = itM->second;
+      }
+  }
+
+
+ AFD(std::list<char> lSimbolos, int cEstados):
+   m_cSimbolos(static_cast<int>(lSimbolos.size())),
+   m_cEstados(cEstados),
+   m_vbEstadosFinales(cEstados),
+   m_vcAlfabeto(static_cast<int>(lSimbolos.size())),
+   m_strName("")
+   {
+     std::list<char>::iterator it = lSimbolos.begin();
+	   for (int i = 0; i < m_cSimbolos; i++, it++) {
+		   m_lciAlfabeto[*it] = i;
+		   m_vcAlfabeto[i] = *it;
+	   }
+     do {
+	    inicializar();
+     } while (m_lTransiciones.empty());
+   }
+
+ 
+  /*
+   * minimizar -> devuelve el AFD mínimo asociado a este AFD
+   */
+  AFD minimizar() const;
+  AFD minimizar2() const;
+
+  /*
+   *
+   */
+  bool evaluar(std::string strCadena) const;
+  
+  /*
+   * Metodo para introducir un alfabeto del automata
+   */
+  void setAlfabeto (std::list<char> lAlfabeto);
+
+  /*
+   * Metodo que devuelbe el afabeto del automata
+   */
+  std::list<char> getAlfabeto () const;
+
+  /*
+   * Permite salvar el AFD a un fichero ASCII
+   */
+  bool salvar(const std::string& strFileName) const;
+
+  std::string getName() const { return m_strName; };
+  void setName(const std::string& strName) { m_strName = strName; }
+
+  std::set<std::set<int> > calculaEstadosDR() const;
+  
+ private:
+	
+  std::string m_strName;
+  int m_cSimbolos; 
+  int m_cEstados; 
+
+  /* 
+   * Función auxiliar que nos permite calcular las intersecciones entre lenguajes por la derecha para
+   * poder calcular el autómata universal
+   */
+
+
+	
+  /*
+   * Mapa de transiciones. Para obtener el destino de una transicion desde un origen, con un
+   * simbolo, hay que acceder al mapa con el par (origen,simbolo) y nos devolvera el destino
+   */
+  std::map<Par, int> m_lTransiciones;
+	
+  /* 
+   * Vector de booleanos, donde final[e] == true indica que el estado e es final,
+   * y no en caso contrario.
+   */
+  std::vector<bool> m_vbEstadosFinales; 
+
+  // Estado inicial del automata.
+  int m_iEstadoInicial;
+  
+  /*
+   * Mapa para traducir al alfabeto de entrada a un entero identificador.
+   */
+  std::map<char, int> m_lciAlfabeto;
+  
+  /*
+   * Mapa para traducir de entero identifcador a simbolo del alfabeto.
+   */
+  std::vector<char> m_vcAlfabeto;
+
+  /*
+   * inicializar -> inicializa el AFD de forma aleatoria.
+   */
+  void inicializar();
+  
+  /*
+   * Comprueba si hay estados no alcanzables
+   */
+  bool hayEstadosNoAlcanzables() const;
+ 
+  /*
+   * Funciones auxiliares para la minimizacion
+   */
+  class Particion {
+  private:
+    typedef std::map<std::list<int>,std::list<int> > t_Particion;
+    t_Particion m_Particion;
+
+  public:
+    typedef std::list<int> t_IDCompartimento;
+    typedef std::list<int> t_ListaEstados;
+    typedef t_Particion::const_iterator const_iterator;
+    typedef t_Particion::iterator iterator;
+
+    void insertarEnCompartimento(const t_IDCompartimento& lIDCompartimento, const int estado);
+    t_IDCompartimento obtenerCompartimento(const int iEstado) const;
+    int obtenerIndice(const int iEstado) const;
+    bool operator ==(const Particion& rhs) const;
+    t_ListaEstados& operator [](const t_IDCompartimento& lIDCompartimento) {
+      return m_Particion[lIDCompartimento];
+    }
+    
+    size_t getSize() const
+    {
+      return m_Particion.size();
+    }
+
+    iterator begin() {
+      return m_Particion.begin();
+    }
+
+    const_iterator begin() const {
+      return m_Particion.begin();
+    }
+
+    iterator end() {
+      return m_Particion.end();
+    }
+
+    const_iterator end() const {
+      return m_Particion.end();
+    }
+  };
+	
+};
+
+#endif /*AFD_H_*/
