2955e5da3e4db503ca4b2456fae4ab6f5241e4a9
cyberslas <cyberslas@5b558abe-654c-0410-8053-5d03c91816a9>
2008-05-21 16:56:47 +0000

    Añadida la clase AFN, para el Autómata Universal, que no es determinista

---
+++ trunk/types.h
@@ -0,0 +1,9 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef std::pair<int,int> Par;
+typedef std::pair<int,char> cPar;
+typedef std::pair<Par, int> Transicion;
+typedef std::pair<cPar, int> cTransicion;
+
+#endif
