e4aff05052cff1d2c13e3691b15430453c993603
cyberslas <cyberslas@5b558abe-654c-0410-8053-5d03c91816a9>
2008-05-21 16:56:47 +0000

    Añadida la clase AFN, para el Autómata Universal, que no es determinista

---
+++ AFN.cpp
@@ -0,0 +1,103 @@
+#include "AFN.h"
+#include <list>
+#include <vector>
+#include <map>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <set>
+#include <queue>
+#include <fstream>
+#include <cmath>
+
+#ifdef WINDOWS
+#define SRANDOM srand
+#define RANDOM rand
+#else
+#define SRANDOM srandom
+#define RANDOM random
+#endif
+
+using namespace std;
+
+
+bool fileExists_(const std::string& fileName)
+{
+  std::fstream fin;
+  fin.open(fileName.c_str(),std::ios::in);
+  if( fin.is_open() )
+  {
+    fin.close();
+    return true;
+  }
+  fin.close();
+  return false;
+}
+
+bool AFN::salvar(const std::string& strFileName) const {
+  std::ofstream osSalida;
+  
+  if (fileExists_(strFileName))
+  {
+    // Si el archivo existe, entonces sólo se pueden añadir AFDs con nombre
+    if(m_strName.empty())
+      return false;
+
+    osSalida.open(strFileName.c_str(),std::ios_base::app);
+  }
+  else
+  {
+    osSalida.open(strFileName.c_str());
+  }
+
+  if(!m_strName.empty())
+    osSalida << "Name " << m_strName << std::endl;
+  
+  osSalida << "NumStates " << m_cEstados << std::endl;
+  
+  for(int iEstado=0; iEstado<m_cEstados; iEstado++)
+    osSalida << "State " << iEstado << " initial=" << (m_iEstadoInicial==iEstado) << " final=" << (m_vbEstadosFinales[iEstado] ? 1:0) << std::endl;
+  
+//  for(int iEstadoOrigen=0; iEstadoOrigen<m_cEstados; iEstadoOrigen++)
+//    for(int iSimbolo=0; iSimbolo<m_cSimbolos; iSimbolo++)
+//      osSalida << iEstadoOrigen << " " << (m_lTransiciones.find(Par(iEstadoOrigen,iSimbolo)))->second << " " << m_vcAlfabeto[iSimbolo] << std::endl;
+
+  std::set<Transicion>  lTransiciones(m_lTransiciones);
+  std::cout << "XD: "<< lTransiciones.size() << std::endl;
+  for(std::set<Transicion>::iterator it = lTransiciones.begin();
+      it != lTransiciones.end();
+      it++) {
+	osSalida << it->first.first << " " << it->second << " " << it->first.second << std::endl;
+  }
+
+  osSalida.close();
+
+  return true;
+}
+
+void AFN::setAlfabeto(std::list<char> lAlfabeto) {
+  /*
+   * El método solo cogera de la lista de simbolos los indicados por el numero de simbolos
+   * que se haya definido en el constructor. Se usará para definir el alfabeto si no se ha 
+   * definido en el constructor.
+   */
+  std::vector<char> vcAlfabeto(static_cast<int>(m_vcAlfabeto.size()));
+  m_lciAlfabeto.clear();
+  m_vcAlfabeto.clear();
+  std::list<char>::iterator it = lAlfabeto.begin();
+  for (int i = 0; i < m_cSimbolos; i++, it++) {
+    m_lciAlfabeto[*it] = i;
+    vcAlfabeto[i] = *it;
+  } 
+  m_vcAlfabeto = vcAlfabeto;
+}
+
+std::list<char> AFN::getAlfabeto () const {
+  std::list<char> lAlfabeto;
+  int nSize =  static_cast<int>( m_vcAlfabeto.size() );
+  for (unsigned int i = 0; i < nSize; i++) {
+    lAlfabeto.push_back(m_vcAlfabeto[i]);
+  }
+  return lAlfabeto;  
+}
---
+++ AFN.h
@@ -0,0 +1,104 @@
+#ifndef AFN_H_
+#define AFN_H_
+
+#define WINDOWS
+
+#include <list>
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+#include <cstdlib>
+#include "types.h"
+
+/*
+ * Clase que implementa un Automata Finito Determinista. 
+ */
+class AFN
+{
+ public:
+
+  /*
+   * El constructor definido requiere:
+   *  cSimbolos="nº de simbolos",
+   *  cEstados="nº de estados"
+   *  lTransiciones="definicion de las transiciones"
+   *  vbEstadosFinales="definición de estados finales"
+   *  
+   */
+ AFN(int cSimbolos, int cEstados, std::set<Transicion> lTransiciones, std::vector<bool> vbEstadosFinales, int iEstadoInicial):
+  m_cSimbolos(cSimbolos),
+    m_cEstados(cEstados),
+    m_vbEstadosFinales(vbEstadosFinales),
+    m_lTransiciones(lTransiciones),
+    m_iEstadoInicial(iEstadoInicial),
+    m_strName("")
+    {
+    }
+
+AFN(std::set<char> lSimbolos, int cEstados, std::set<cTransicion> lTransiciones, std::vector<bool> vbEstadosFinales, int iEstadoInicial):
+  m_cSimbolos(static_cast<int>(lSimbolos.size())),
+    m_cEstados(cEstados),
+    m_vbEstadosFinales(vbEstadosFinales),
+    m_iEstadoInicial(iEstadoInicial),
+    m_vcAlfabeto(static_cast<int>(lSimbolos.size()))
+    {
+      std::set<char>::iterator it = lSimbolos.begin();
+      for (int i = 0; i < m_cSimbolos; i++, it++) {
+        m_lciAlfabeto[*it] = i;
+        m_vcAlfabeto[i] = *it;
+      }
+      std::set<cTransicion>::iterator itM;
+      for (itM = lTransiciones.begin(); itM != lTransiciones.end(); itM++) {
+        m_lTransiciones.insert(Transicion(Par((itM->first).first, m_lciAlfabeto[(itM->first).second]), itM->second ));
+      }
+  }
+
+   int getNumEstados() {return m_cEstados;}
+ 
+  void setAlfabeto (std::list<char> lAlfabeto);
+  std::list<char> getAlfabeto () const;
+
+  /*
+   * Permite salvar el AFD a un fichero ASCII
+   */
+  bool salvar(const std::string& strFileName) const;
+
+  std::string getName() const { return m_strName; };
+  void setName(const std::string& strName) { m_strName = strName; }
+  
+
+ private:
+	
+  std::string m_strName;
+  int m_cSimbolos; 
+  int m_cEstados; 
+
+  /*
+   * Mapa de transiciones. Para obtener el destino de una transicion desde un origen, con un
+   * simbolo, hay que acceder al mapa con el par (origen,simbolo) y nos devolvera el destino
+   */
+  std::set<Transicion> m_lTransiciones;
+	
+  /* 
+   * Vector de booleanos, donde final[e] == true indica que el estado e es final,
+   * y no en caso contrario.
+   */
+  std::vector<bool> m_vbEstadosFinales; 
+
+  // Estado inicial del automata.
+  int m_iEstadoInicial;
+  
+  /*
+   * Mapa para traducir al alfabeto de entrada a un entero identificador.
+   */
+  std::map<char, int> m_lciAlfabeto;
+  
+  /*
+   * Mapa para traducir de entero identifcador a simbolo del alfabeto.
+   */
+  std::vector<char> m_vcAlfabeto;
+
+};
+
+#endif /*AFN_H_*/
